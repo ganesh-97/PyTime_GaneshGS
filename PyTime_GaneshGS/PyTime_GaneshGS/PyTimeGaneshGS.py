2fcb3298a1275f0daefcac608440c5decbccc7ab
Ganesh G.S <ganeshgs1691997@gmail.com>
2019-03-31 11:33:04 +0530

    Update PyTimeGaneshGS.py

--- PyTime_GaneshGS/PyTime_GaneshGS/PyTimeGaneshGS.py
+++ PyTime_GaneshGS/PyTime_GaneshGS/PyTimeGaneshGS.py
@@ -2,36 +2,28 @@ class PyTime:
     
     def printTime(self,input):
         time_in_string = str(input)
-        length = len(time_in_string)
-        if length == 3:
-            hour,minute1,minute2 = time_in_string
-            hours = "0" + hour
-            minutes = minute1 + minute2
-            converted_time = self.calculateTime(hours,minutes)
-        elif length == 4:
-            hour1,hour2,minute1,minute2 =  time_in_string
-            hours = hour1 + hour2
-            minutes = minute1 + minute2
-            converted_time = self.calculateTime(hours,minutes)
-        elif time_in_string=="000" or time_in_string=="0000":
-            hour1,hour2,minute1,minute2 =  time_in_string
-            hours = hour1 + hour2
-            minutes = minute1 + minute2
+        if time_in_string == "0" or time_in_string == "00" or time_in_string == "000" or time_in_string == "0000":
+            hours = 0
+            minutes = 0
             converted_time = self.calculateTime(hours,minutes)
         else:
-            converted_time = "Invalid Format"
+            time_in_int = int(time_in_string)
+            if time_in_int < 1441:
+                hours = int(time_in_int/60)
+                minutes = time_in_int%60
+                converted_time = self.calculateTime(hours,minutes)
+            else:
+                converted_time = "Invalid Time Format"
         return converted_time
 
     def calculateTime(self,hours,minutes):
-        hours_in_int = int(hours)
-        minutes_in_int = int(minutes)
-        if hours_in_int >= 1 and hours_in_int < 12:
-            converted_time =  hours + ":" + minutes + " AM"
-        elif hours_in_int >= 12 and hours_in_int < 24:
-            converted_time =  hours + ":" + minutes + " PM"
-        elif hours == "00":
-            hours  = "12"
-            converted_time = hours + ":" + minutes + " AM"
-        elif hours_in_int >= 24:
-            converted_time = "Invalid Format"
+        if minutes < 10:
+            minutes = "0" + str(minutes)
+        if hours >= 1 and hours < 12:
+            converted_time =  str(hours) + ":" + str(minutes)
+        elif hours >= 12 and hours < 24:
+            converted_time =  str(hours) + ":" + str(minutes)
+        elif hours == 0 or hours == 24:
+            hours  = 12
+            converted_time = str(hours) + ":" + str(minutes) + " AM"
         return converted_time 
